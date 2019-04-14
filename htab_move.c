22f7f83413fdb2730e66a3e76e9f8b75abc41d2d
PeterVinarcik <peter.vinarcik99@gmail.com>
2019-04-14 19:00:49 +0200

    Edited clear, free, move and updated style of hash function

---
+++ htab_move.c
@@ -0,0 +1,24 @@
+#include "htab.h"
+#include "htab_structures.h"
+
+htab_t *htab_move(size_t n, htab_t *from)
+{
+    int i = 0;
+    struct htab_item *temp;
+    htab_t *t;
+    t = htab_init(n);
+    size_t iterator = from->arr_size;
+
+    while (i < iterator){
+        temp = from->ptr[i];
+        while(temp != NULL){
+            t->ptr[i] = temp;
+            temp = temp->next;
+        }
+        i++;
+    }
+
+    htab_clear(from);
+
+    return t;
+}
